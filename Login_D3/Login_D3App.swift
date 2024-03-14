976d3072aecf0cdd30c4cff8daae5c52a707c627
sirx2713 <bradx2713@gmail.com>
2024-03-14 23:02:13 +0300

    Initial Commit

---
+++ Login_D3/Login_D3App.swift
@@ -0,0 +1,17 @@
+//
+//  Login_D3App.swift
+//  Login_D3
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Login_D3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
